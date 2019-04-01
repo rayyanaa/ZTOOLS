4415f0d111f92ed76df4441d2bc7137237bc3211
rayyanaa <47921394+rayyanaa@users.noreply.github.com>
2019-04-01 08:54:46 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,25 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+clear
+echo $pur"Tunggu 2Detik"
+sleep 2
+echo $i"Tunggu 1Detik"
+sleep 2
+rm -rf ZTOOLS
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/rayyanaa/ZTOOLS
+echo $cy"Semoga Bermanfaat :)
+
+
+
+
+
+
